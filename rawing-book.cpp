d2dd27d55e4b61ee5188e519133b873edb367667
TraiLoveLinh <136947709+TraiLoveLinh@users.noreply.github.com>
2023-08-27 15:03:24 +0000

    modified:   rawing-book.cpp

--- rawing-book.cpp
+++ rawing-book.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 int main()
 {
+    int n,p;
+    cin>>n>>p;
+    if (p==1||p==n)
+        cout<<0<<endl;
+    else if(true)
+    {
+        cout<<abs(n-p)<<endl;
+    }
     return 0;
 }
