8c513d7fd51f6ab8470fae6148a2b8e73bc9c559
TraiLoveLinh <traipupgmobile@gmail.com>
2023-08-28 10:35:21 +0700

    .vscode/c_cpp_properties.json Divisible Sum Pairs.cpp Divisible Sum Pairs.exe Subarray Division.cpp bro.cpp bro.exe migratory-birds.cpp migratory-birds.exe

--- btw2set.cpp
+++ btw2set.cpp
@@ -1,58 +1,38 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
-
+#include <vector>
 using namespace std;
-
-int checkA(vector<int>a, int n, int k)
-{  
-    for(int i = 0; i < n; i++)
+bool checkA(vector<int>& a, int n, int k)
+{
+    for(int i=0;i<n;i++)
         if(k%a[i])
             return 0;
     return 1;
 }
-int checkB(vector<int>b, int n, int k)
+bool checkB(vector<int>& b, int m, int k)
 {
-    for(int i = 0; i < n; i++)
+    for(int i=0;i<m;i++)
+    {
         if(b[i]%k)
             return 0;
+    }
     return 1;
 }
-
 int main()
-{   
+{
     int n,m;
     cin>>n>>m;
     vector<int>a(n);
     vector<int>b(m);
-    for(int i_a=0;i_a<n;i_a++)
-        cin>>a[i_a];
-    for(int i_b=0;i_b<m;i_b++)
-        cin>>b[i_b];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    for(int i=0;i<m;i++)
+        cin>>b[i];
     int cnt=0;
     for(int i=1;i<=100;i++)
         if(checkA(a,n,i)&&checkB(b,m,i))
             cnt++;
-    cout<<cnt;
-    // system("pause");
+    cout<<cnt<<endl;
+    system("pause");
+    
     return 0;
 }
