913c6a677dbee3002c0045398ca9dc4df7ea9450
TraiLoveLinh <traipupgmobile@gmail.com>
2023-09-01 21:09:41 +0700

    modified:   cats-and-a-mouse modified:   cats-and-a-mouse.cpp new file:   magic-square-forming new file:   magic-square-forming.cpp new file:   xaa

--- cats-and-a-mouse.cpp
+++ cats-and-a-mouse.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout<<"Hello world"<<endl;
+  #include <iostream>
+  using namespace std;
+  int main()
+  {
+    int q,x,y,z;
+    cin>>q;
+    for(int i=0;i<q;i++)
+    {
+      cin>>x>>y>>z;
+      if(abs(x-z)>abs(y-z))
+        cout<<"Cat A"<<endl;
+      else if(abs(y-z)<abs(x-z))
+        cout<<"Cat B"<<endl;
+      else
+        cout<<"Mouse C"<<endl;
+    }
     return 0;
-}
+  }
